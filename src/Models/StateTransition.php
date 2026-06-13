3b890b47cf43da0d81298ad764346c6315ac2d35
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-13 17:47:13 +0400

    [AEGISORA-11] Implmeneted attributes.

--- src/Models/StateTransition.php
+++ src/Models/StateTransition.php
@@ -4,4 +4,14 @@ namespace Aegisora\Rules\StateTransition\Models;
 
 class StateTransition
 {
+    private State $from;
+    private State $to;
+
+    public function __construct(
+        State $from,
+        State $to
+    ) {
+        $this->from = $from;
+        $this->to = $to;
+    }
 }
