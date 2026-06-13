556bfc3d0223f26469370c5de9037a0653f7bc24
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-13 15:01:28 +0400

    [AEGISORA-11] Implemented name attribute.

--- src/Models/State.php
+++ src/Models/State.php
@@ -4,4 +4,11 @@ namespace Aegisora\Rules\StateTransition\Models;
 
 class State
 {
+    private string $name;
+
+    public function __construct(
+        string $name
+    ) {
+        $this->name = $name;
+    }
 }
