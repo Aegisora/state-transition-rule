964cbb4aef746258b4c4f91b4c886b8fc2658285
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-14 16:42:06 +0400

    [AEGISORA-11] Implemented assertActualStateTransitionEqualsExpected.

--- tests/Unit/Models/StateTransitionTest.php
+++ tests/Unit/Models/StateTransitionTest.php
@@ -2,8 +2,16 @@
 
 namespace Aegisora\Rules\StateTransition\Tests\Unit\Models;
 
+use Aegisora\Rules\StateTransition\Models\StateTransition;
 use PHPUnit\Framework\TestCase;
 
 class StateTransitionTest extends TestCase
 {
+    private static function assertActualStateTransitionEqualsExpected(
+        StateTransition $actual,
+        array $expectedData
+    ): void {
+        self::assertSame($expectedData['from']['name'], $actual->getFrom()->getName());
+        self::assertSame($expectedData['to']['name'], $actual->getTo()->getName());
+    }
 }
