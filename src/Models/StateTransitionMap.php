f13e32623f4197b2e44a6a89b078d22475429fac
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-14 16:59:37 +0400

    [AEGISORA-11] Implemented attributes.

--- src/Models/StateTransitionMap.php
+++ src/Models/StateTransitionMap.php
@@ -4,4 +4,24 @@ namespace Aegisora\Rules\StateTransition\Models;
 
 class StateTransitionMap
 {
+    private State $sourceState;
+
+    /** @var State[] */
+    private array $transitionStates = [];
+
+    /**
+     * @param State[] $transitionStates
+     */
+    public function __construct(
+        State $sourceState,
+        array $transitionStates
+    ) {
+        $this->sourceState = $sourceState;
+        $this->transitionStates = array_filter(
+            $transitionStates,
+            static function ($transitionState): bool {
+                return $transitionState instanceof State;
+            }
+        );
+    }
 }
