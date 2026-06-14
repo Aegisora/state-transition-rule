ae8f8b5b7338521862b581d5962038c113d931ac
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-14 17:07:41 +0400

    [AEGISORA-11] Implemented testCreate.

--- tests/Unit/Models/StateTransitionMapTest.php
+++ tests/Unit/Models/StateTransitionMapTest.php
@@ -7,6 +7,28 @@ use PHPUnit\Framework\TestCase;
 
 class StateTransitionMapTest extends TestCase
 {
+    /**
+     * @dataProvider getCreateStateTransitionMapProvidedData
+     */
+    public function testCreate(
+        array $actualData,
+        array $expectedData
+    ): void {
+        self::assertActualStateTransitionMapEqualsExpected(
+            new StateTransitionMap(...array_values($actualData)),
+            $expectedData
+        );
+        self::assertActualStateTransitionMapEqualsExpected(
+            StateTransitionMap::create(...array_values($actualData)),
+            $expectedData
+        );
+    }
+
+    public static function getCreateStateTransitionMapProvidedData(): array
+    {
+        return [];
+    }
+
     private static function assertActualStateTransitionMapEqualsExpected(
         StateTransitionMap $actual,
         array $expectedData
