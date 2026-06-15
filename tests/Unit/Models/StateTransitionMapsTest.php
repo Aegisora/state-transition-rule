ca35fd7d837d45fe59c5b278fd9c638d67f7fab1
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-15 20:20:14 +0400

    [AEGISORA-11] Implemented assertActualStateTransitionMapsEqualsExpected.

--- tests/Unit/Models/StateTransitionMapsTest.php
+++ tests/Unit/Models/StateTransitionMapsTest.php
@@ -2,8 +2,15 @@
 
 namespace Aegisora\Rules\StateTransition\Tests\Unit\Models;
 
+use Aegisora\Rules\StateTransition\Models\StateTransitionMaps;
 use PHPUnit\Framework\TestCase;
 
 class StateTransitionMapsTest extends TestCase
 {
+    private static function assertActualStateTransitionMapsEqualsExpected(
+        StateTransitionMaps $actual,
+        array $expectedData
+    ): void {
+        self::assertEquals($expectedData['maps'], $actual->getMaps());
+    }
 }
