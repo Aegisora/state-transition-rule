8a4733769515cc3fd4ab376e9e70b8064e5ed42d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-15 20:18:31 +0400

    [AEGISORA-11] Implemented createFromArray.

--- src/Models/StateTransitionMaps.php
+++ src/Models/StateTransitionMaps.php
@@ -22,6 +22,45 @@ class StateTransitionMaps
         );
     }
 
+    public static function createFromArray(array $rawData): self
+    {
+        $maps = [];
+
+        foreach ($rawData as $stateTransitionMapRawData) {
+            if (!is_array($stateTransitionMapRawData)) {
+                continue;
+            }
+
+            foreach ($stateTransitionMapRawData as $sourceStateName => $transitionStatesRawData) {
+                if (!is_string($sourceStateName)) {
+                    continue;
+                }
+
+                if (empty($sourceStateName)) {
+                    continue;
+                }
+
+                $transitionStates = [];
+
+                foreach ($transitionStatesRawData as $transitionStateRawData) {
+                    if (!is_string($transitionStateRawData)) {
+                        continue;
+                    }
+
+                    if (empty($transitionStateRawData)) {
+                        continue;
+                    }
+
+                    $transitionStates[] = State::create($transitionStateRawData);
+                }
+
+                $maps[] = StateTransitionMap::create(State::create($sourceStateName), $transitionStates);
+            }
+        }
+
+        return self::create($maps);
+    }
+
     /**
      * @param StateTransitionMap[] $maps
      */
