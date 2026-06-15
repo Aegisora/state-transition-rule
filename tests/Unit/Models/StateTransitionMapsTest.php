9745a3d5984e070b7e1c7645d414c6d5bf12e634
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-15 20:41:21 +0400

    [AEGISORA-11] Covered: raw data - with invalid elements.

--- tests/Unit/Models/StateTransitionMapsTest.php
+++ tests/Unit/Models/StateTransitionMapsTest.php
@@ -6,6 +6,7 @@ use Aegisora\Rules\StateTransition\Models\State;
 use Aegisora\Rules\StateTransition\Models\StateTransitionMap;
 use Aegisora\Rules\StateTransition\Models\StateTransitionMaps;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class StateTransitionMapsTest extends TestCase
 {
@@ -95,6 +96,32 @@ class StateTransitionMapsTest extends TestCase
                         new StateTransitionMap(new State('StateA'), []),
                         new StateTransitionMap(new State('StateB'), [new State('StateD'), new State('StateC'), new State('StateE'),]),
                         new StateTransitionMap(new State('StateC'), [new State('StateD'),]),
+                    ],
+                ],
+            ],
+            'raw data - with invalid elements' => [
+                'rawData' => [
+                    ['StateA' => [],],
+                    ['StateB' => ['StateD', 'StateC', 'StateE',],],
+                    ['StateB' => ['StateD', 'StateC', 'StateE',],],
+                    [1 => ['StateD', 'StateC', 'StateE',],],
+                    [0 => ['StateD', 'StateC', 'StateE',],],
+                    [0.1 => ['StateD', 'StateC', 'StateE',],],
+                    [true => ['StateD', 'StateC', 'StateE',],],
+                    [false => ['StateD', 'StateC', 'StateE',],],
+                    ['' => ['StateD', 'StateC', 'StateE',],],
+                    ['StateC' => ['StateD'],],
+                    ['StateD' => ['StateA', 0, 1, 'StateA', 0.1, 'StateB', true, 'StateC', false, '', new stdClass(), 'StateD', ''],],
+                    ['StateD' => ['StateA', 0, 1, 'StateA', 0.1, 'StateB', true, 'StateC', false, '', new stdClass(), 'StateD', ''],],
+                ],
+                'expectedData' => [
+                    'maps' => [
+                        new StateTransitionMap(new State('StateA'), []),
+                        new StateTransitionMap(new State('StateB'), [new State('StateD'), new State('StateC'), new State('StateE'),]),
+                        new StateTransitionMap(new State('StateB'), [new State('StateD'), new State('StateC'), new State('StateE'),]),
+                        new StateTransitionMap(new State('StateC'), [new State('StateD'),]),
+                        new StateTransitionMap(new State('StateD'), [new State('StateA'), new State('StateA'), new State('StateB'), new State('StateC'), new State('StateD'),]),
+                        new StateTransitionMap(new State('StateD'), [new State('StateA'), new State('StateA'), new State('StateB'), new State('StateC'), new State('StateD'),]),
                     ],
                 ],
             ],
