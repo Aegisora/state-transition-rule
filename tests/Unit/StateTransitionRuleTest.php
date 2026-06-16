3829fcaf9988be567f2b582a19c18c8519ae54b5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-16 20:21:16 +0400

    [AEGISORA-11] Covered: create rule.

--- tests/Unit/StateTransitionRuleTest.php
+++ tests/Unit/StateTransitionRuleTest.php
@@ -2,8 +2,15 @@
 
 namespace Aegisora\Rules\StateTransition\Tests\Unit;
 
+use Aegisora\RuleContract\RuleInterface;
+use Aegisora\Rules\StateTransition\Models\StateTransitionMaps;
+use Aegisora\Rules\StateTransition\StateTransitionRule;
 use PHPUnit\Framework\TestCase;
 
 class StateTransitionRuleTest extends TestCase
 {
+    public function testCreate(): void
+    {
+        self::assertInstanceOf(RuleInterface::class, StateTransitionRule::create(StateTransitionMaps::create([])));
+    }
 }
