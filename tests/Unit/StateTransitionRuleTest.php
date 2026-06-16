f803c23dc020f25ed997219a9d0b65ed3f35a8e9
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-16 20:26:13 +0400

    [AEGISORA-11] Implemented testValidateFailedCauseInvalidContext.

--- tests/Unit/StateTransitionRuleTest.php
+++ tests/Unit/StateTransitionRuleTest.php
@@ -2,9 +2,12 @@
 
 namespace Aegisora\Rules\StateTransition\Tests\Unit;
 
+use Aegisora\RuleContract\Exceptions\InvalidRuleContextException;
+use Aegisora\RuleContract\Models\Context;
 use Aegisora\RuleContract\RuleInterface;
 use Aegisora\Rules\StateTransition\Models\StateTransitionMaps;
 use Aegisora\Rules\StateTransition\StateTransitionRule;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class StateTransitionRuleTest extends TestCase
@@ -12,5 +15,33 @@ class StateTransitionRuleTest extends TestCase
     public function testCreate(): void
     {
         self::assertInstanceOf(RuleInterface::class, StateTransitionRule::create(StateTransitionMaps::create([])));
+    }
+
+    /**
+     * @dataProvider getInvalidContextProvidedData
+     */
+    public function testValidateFailedCauseInvalidContext(
+        $contextValue
+    ): void {
+        $this->expectException(InvalidRuleContextException::class);
+
+        StateTransitionRule::create(StateTransitionMaps::create([]))->validate($this->getContextMock(['value' => $contextValue,]));
+    }
+
+    public static function getInvalidContextProvidedData(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return Context|MockObject
+     */
+    private function getContextMock(array $params = []): Context
+    {
+        /** @var Context|MockObject $mock */
+        $mock = $this->createMock(Context::class);
+        $mock->method('getValue')->willReturn($params['value'] ?? null);
+
+        return $mock;
     }
 }
