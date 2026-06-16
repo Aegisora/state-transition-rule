a2470f40456c8ff50f12e9067855dad172643450
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-06-16 19:53:28 +0400

    [AEGISORA-11] Implemented attribute.

--- src/StateTransitionRule.php
+++ src/StateTransitionRule.php
@@ -2,6 +2,15 @@
 
 namespace Aegisora\Rules\StateTransition;
 
+use Aegisora\Rules\StateTransition\Models\StateTransitionMaps;
+
 class StateTransitionRule
 {
+    private StateTransitionMaps $allowedTransitions;
+
+    public function __construct(
+        StateTransitionMaps $allowedTransitions
+    ) {
+        $this->allowedTransitions = $allowedTransitions;
+    }
 }
